a025ec71b774faf57c541a024c1857e783f3e6c8
unknown <dyomin.dyominalexander2011@yandex.ru>
2020-09-02 10:33:56 +0300

    Clients are added

--- script/script.js
+++ script/script.js
@@ -2,16 +2,14 @@ function UpdateNavbar()
 {
     let navbar = document.getElementById('navbar');
     let duration = 500;
-    let opacity = getComputedStyle(navbar).opacity;
-    opacity = Number(opacity);
-    console.log(opacity);
+    let opacity = Number(getComputedStyle(navbar).opacity);
     if (document.documentElement.scrollTop > 50 && opacity == 0) {
         navbar.style.opacity = '1';
         navbar.classList.add('fade-in');
         setTimeout(() => {
             navbar.classList.remove('fade-in');
         }, duration);
-    } else if (document.documentElement.scrollTop == 0) {
+    } else if (document.documentElement.scrollTop == 0 && opacity == 1) {
         navbar.style.opacity = '0';
         navbar.classList.add('fade-away');
         setTimeout(() => {
@@ -20,10 +18,23 @@ function UpdateNavbar()
     }
 }
 
+function CreateNavbar()
+{
+    let nav_btn = document.getElementById('navbar-button');
+    let nav = document.getElementById('navbar').getElementsByClassName('nav')[0];
+    nav_btn.addEventListener('click', e => {
+        if (getComputedStyle(nav).visibility == 'hidden')
+            nav.style.visibility = 'visible';
+        else
+            nav.style.visibility = 'hidden';
+    });
+}
+
 window.onscroll = function() {
     UpdateNavbar();
 }
 
 function OnLoadFunction()
 {
+    CreateNavbar();
 }
